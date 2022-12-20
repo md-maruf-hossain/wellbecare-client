f0cdc6763fc1a28ee8652f06a92473b81cef3a6f
Maruf Hossain <mdmarufhossain6470@gmail.com>
2022-12-20 15:37:45 +0600

    price added

--- src/Pages/Appointments/AvailableAppointment/AppointmentOption.js
+++ src/Pages/Appointments/AvailableAppointment/AppointmentOption.js
@@ -1,20 +1,27 @@
 import React from "react";
 
 const AppointmentOption = ({ appointmentOption, setTreatment }) => {
-  const { name, slots } = appointmentOption;
+  const { name, price, slots } = appointmentOption;
   return (
     <div className="card shadow-xl my-10">
       <div className="card-body text-center">
         <h2 className="text-sec text-2xl font-semibold">{name}</h2>
         <p>{slots.length > 0 ? slots[0] : "Try another Day"}</p>
-        <p>{slots.length} {slots.length > 1 ? "spaces" : "space"} available. </p>
+        <p>
+          {slots.length} {slots.length > 1 ? "spaces" : "space"} available.{" "}
+        </p>
+        <p>
+          Price: $<span>{price}</span>
+        </p>
         <div className="card-actions justify-center">
-          <label 
-            htmlFor="booking" 
+          <label
+            htmlFor="booking"
             disabled={slots.length === 0}
             className="btn btn-primary text-white"
-            onClick={()=> setTreatment(appointmentOption)}
-            >Book Appointment</label>
+            onClick={() => setTreatment(appointmentOption)}
+          >
+            Book Appointment
+          </label>
         </div>
       </div>
     </div>
